e389691417f7f094ab3cb5b476c86ae9dca128db
Asmaa-abdulkhaliq <aae3728@gmail.com>
2025-08-25 13:11:09 +0300

    first upload for flutter task

--- my_app/lib/Gradient_Container.dart
+++ my_app/lib/Gradient_Container.dart
@@ -1,5 +1,59 @@
 
+import'package:flutter/material.dart';
 
+
+
+class GradientContainer extends StatelessWidget {
+GradientContainer({super.key});
+@override
+Widget build(context){
+return Container(
+  decoration: const BoxDecoration(
+    gradient: LinearGradient(
+      colors: [
+Color.fromARGB(255, 135, 222, 195),
+Color.fromARGB(197, 89, 210, 210),
+Color.fromARGB(108, 59, 156, 175),
+Color.fromARGB(65, 11, 123, 110),
+
+
+
+
+
+      ],
+       begin: Alignment.topRight,
+          end: Alignment.bottomRight
+  ),
+),
+child: const Center(child: Text('Hi',style: TextStyle(
+      color: Colors. white,
+      fontSize: 80,
+    ),),),);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 33, 150, 243),
+                const Color.fromARGB(255, 3, 169, 244),  
+                const Color.fromARGB(255, 0, 188, 212),  
+                const Color.fromARGB(255, 0, 230, 118),  
+                const Color.fromARGB(255, 139, 195, 74),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white, 
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,38 +40,66 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Background(), 
-    );
-  }
-}
-
-class Background extends StatelessWidget {
-  const Background({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue, //
-      body: const Center(
-        child: StyledText(), //
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Hello', 
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 80,
-        fontWeight: FontWeight.bold,
+   
+    return Scaffold(
+      appBar: AppBar(
+       
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+        title: Text(widget.title),
+      ),
+      body: Center(
+       
+        child: Column(
+         
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
